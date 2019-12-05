953b346a4ae56acfcc3a0725ec3ebcb64b567457
David Navarro Roncero <david.navarroroncero@altran.es>
2019-12-05 13:34:32 +0100

    SQL create bbdd

---
+++ sql/create_bbdd.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Usuarias (
+   id_user INTEGER PRIMARY KEY,
+   nombre TEXT,
+   apellido_1 TEXT,
+   apellido_2 TEXT,
+   nacionalidad TEXT,
+   nacionalidad_2 TEXT,
+   pais_origen TEXT,
+   ciudad_origen TEXT,
+   direccion TEXT,
+   codigo_postal TEXT,
+   ciudad_actual TEXT, 
+   edad INTEGER,
+   estado_civil TEXT,
+   ciudad TEXT,
+   num_telefono INTEGER   
+);
+
